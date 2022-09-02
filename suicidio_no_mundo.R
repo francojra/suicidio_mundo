1afb4f17c3076228f0617868e58e830e3a6caf05
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2022-09-01 23:18:33 -0300

    Add files via upload

---
+++ suicidio_no_mundo.R
@@ -0,0 +1,29 @@
+
+# Suicídio ------------------------------------------------------------------------
+# Autora do script: Jeanne Franco -------------------------------------------------
+# Data: 01/09/22 ------------------------------------------------------------------
+# Referência: https://ourworldindata.org/suicide ----------------------------------
+
+# Sobre os dados ---------------------------------------------------------------------------------------------------------------------------
+
+### Mortes por suicídio é uma questão extremamente complexa que causa dor a
+### centenas de milhares de pessoas todos os anos em volta do mundo. O objetivo
+### desses dados é contribuir para um debate informativo e aberto sobre formas
+### para prevenir o suicídio. Se você tem pensamento suicida, você pode receber
+### ajuda imediata visitando o site Suicide.org.
+
+### Cada suicídio é uma tregédia. A Organização Mundial de Saúde e o Global
+### Burden of Disease estimam que quase 800.000 pessoas morrem de suicídio 
+### a cada ano. Isso é uma pessoa a cada 40 segundos.
+
+### Com intervenções atempadas e baseadas em provas, os suicídios podem ser
+### evitados.
+
+# Carregar pacotes -------------------------------------------------------------------------------------------------------------------------
+
+library(tidyverse)
+
+# Carregar dados ---------------------------------------------------------------------------------------------------------------------------
+
+
+
